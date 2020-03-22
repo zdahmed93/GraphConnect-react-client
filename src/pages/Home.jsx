8d79ef506d982db7c13e9fae3f6b589569819880
zdahmed93 <ahmedzribiahmed@gmail.com>
2020-03-22 19:59:18 +0100

    set up Material UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Button from '@material-ui/core/Button'
 
 export default function Home() {
     return (
         <div>
             <h1>Home page</h1>
+            <Button variant="contained" color="primary">Click</Button>
         </div>
     )
 }
