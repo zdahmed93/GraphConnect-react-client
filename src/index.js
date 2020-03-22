3a874231d4e037712a443f8b72481ec2ba352919
zdahmed93 <ahmedzribiahmed@gmail.com>
2020-03-22 16:54:48 +0100

    set up Apollo Client ecosystem

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App.jsx';
+import ApolloProvider from './ApolloProvider.jsx';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.render(ApolloProvider,
   document.getElementById('root')
 );
 
